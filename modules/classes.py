925414d438f6dc39737bfbd33ac4f2ffe3a29238
youniverse <youniverse@youniversedeMacBook-Pro.local>
2025-03-02 18:05:39 +0800

    update

---
+++ modules/classes.py
@@ -0,0 +1,43 @@
+
+
+
+MAPPING = {
+    "Installation Difficulty": "安装难度" ,
+    "Manual Clarity": "说明书内容" ,
+    "Installation Time": "安装时长" ,
+    "Ease of Moving": "搬运难度" ,
+    "Packaging Protection": "包装保护性" ,
+    "Drawer Slide Smoothness": "抽屉滑轨顺畅度" ,
+    "Drawer Size": "抽屉大小" ,
+    "Overall Storage Space": "整体收纳空间" ,
+    "Wrong or Misfit Parts": "错件情况" ,
+    "Extra Power/USB Sockets": "附加电源USB插座" ,
+    "Cabinet Door Smoothness": "柜门开合顺畅度" ,
+    "Customer Service Quality": "客服质量" ,
+    "Screw Hole Alignment": "螺丝孔位匹配度" ,
+    "Odor": "气味情况" ,
+    "Missing Parts": "缺件情况" ,
+    "Noise During Use": "使用噪音" ,
+    "Legroom Space": "腿部空间大小" ,
+    "Hardware Damage": "五金件损坏情况" ,
+    "Shipping Speed": "物流速度" ,
+    "Table Surface Damage": "桌体破损情况" ,
+    "Tabletop Material": "桌板材质" ,
+    "Tabletop Thickness": "桌板厚度" ,
+    "Water Resistance": "桌面防水性" ,
+    "Anti-Slip": "防滑性" ,
+    "Stability": "稳定性" ,
+    "Aesthetics": "美观度" ,
+    "Edge Finishing": "桌面边缘处理" ,
+    "Number of Assembly Parts": "需拼装组件数量" ,
+    "Surface Material": "桌面材质" ,
+    "Ease of Cleaning": "易清洁性质" ,
+    "Stain Resistance": "耐脏性" ,
+    "Screw Appearance": "螺丝钉外观" ,
+    "Cable Management": "线缆管理" ,
+    "Metal Part Deformation": "金属零部件变形情况" ,
+    "Panel Alignment": "多板平齐情况" ,
+    "Return Shipping Cost": "退货运输费用" ,
+    "Shipment Condition": "运输货品完好度"
+
+}
