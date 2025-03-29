0db47280cef8fb2659d0cbb5def8654608cd933f
youniverse <youniverse@youniversedeMacBook-Pro.local>
2025-03-29 22:06:50 +0800

    update

--- modules/agent.py
+++ modules/agent.py
@@ -10,33 +10,48 @@ class CommentAnalysisAgent:
     def __init__(self, criteria):
         self.points = POINTS[criteria]
         self.comment_analyze_prompt = self.get_comment_analyze_prompt()
-
-    def preprocess(self, text):
-        text = text.replace('\n', ' ')
-        text = text.replace('"', "")
-        return text
-    
-    def comment_analyze(self, comment):
-        comment = self.preprocess(comment)
-        task_text = self.comment_analyze_prompt + '"' + comment + '"; 输出：'
-        response = self.one_shot(task_text)
-        return response
+        self.points_extract_prompt = self.get_points_extract_prompt()
 
     def get_comment_analyze_prompt(self):
-        english_points = str(self.points)
-        english_prompt = '''You are a review analyst. Your task is to extract aspects from customer reviews, which can be positive, negative or neutral. Follow the order in which the points are mentioned in the review and output whether each point is a positive, negative or neutral aspect along with the review angle. The output format should be:{'Pos1': 'xxx', ..., 'PosN': 'xxx', 'Neg1': 'xxx', ..., 'NegN': 'xxx', 'Neu1': 'xxx', 'NeuN': 'xxx'}, If there are no relevant points, output `{}`.
-- The angles for extracting positive and negative review points include: ''' + english_points + '''
-- A review should have a clear emotional bias to be considered positive or negative. A statement like "sturdy" should be considered for its positive aspect of "stability", but a statement like "it's is nice" should not.
+        points = str(self.points)
+        prompt = '''You are a review analyst. Your task is to extract aspects from customer reviews, which can be positive, negative or neutral. Follow the order in which the points are mentioned in the review and output whether each point is a positive, negative or neutral aspect along with the review angle. The output format should be:{'Pos1': 'xxx', ..., 'PosN': 'xxx', 'Neg1': 'xxx', ..., 'NegN': 'xxx', 'Neu1': 'xxx', 'NeuN': 'xxx'}, If there are no relevant points, output `{}`.
+- The angles for extracting positive and negative review points include: ''' + points + '''
+- A review should have a clear emotional bias to be considered positive or negative. A statement like "sturdy" should be considered for its positive aspect of "stability", but a statement like "it's nice" should not.
 Example1: comment="Wonderful desk! Wonderful people to work with. Thank you"; output={}
 Example2: comment="It's a great looking product once it is all put together. There are a lot of screws, so be careful when screwing the frame to the desk top panels to prevent screws penetrating through the panels. Double check the screw sizes."; output={'Pos1': 'Aesthetics', 'Neu1': 'Number of Assembly Parts'}
 Example3: comment="The desk is beautiful looking although it is a little bit deceiving. Kinda wish I ordered a different one. I agree with other reviews that the set up takes forever and there are SO MANY PARTS! I am petite/5'3 and bought a computer chair that is equivalent to the height of a dining room table chair, nothing special and I have a hard time putting my legs under so I do wish it was taller. Also, the drawers aren't as deep as the desk is so that's a bummer they are so small. It's just big enough to fit a 8 x 10 sheet of paper. Overall looks beautiful but functionality can be improved."; output={'Pos1': 'Aesthetics', 'Neg1': 'Installation Time', 'Neg2': 'Number of Assembly Parts', 'Neg3': 'Legroom Space', 'Neg4': 'Storage Space'}
 !Extract only the points explicitly mentioned in the comments, without making any assumptions or inferences. Show the reasoning process without break line and then output the extracted aspects.
 Now do: comment='''
-        return english_prompt
+        return prompt
+
+    def get_points_extract_prompt(self):
+        prompt = '''You are a review analyst. You task is to extract customers' experience points from customer reviews. These are all reviews for a desk. The answer format should be {'point1': 'simple description of what reviewer talked', ...}.
+- When categorizing user feedback on experience points, the definitions should be as specific and informative as possible. For example, a comment like "This table is good" is too vague and lacks specific information. In contrast, "This table looks good" provides a clear point, indicating the table's aesthetic appeal. Therefore, clear definitions can help us better understand the users' actual experiences.
+Example1: comment="Wonderful desk! Wonderful people to work with. Thank you"; output={}
+Example2: comment="It's a great looking product once it is all put together. There are a lot of screws, so be careful when screwing the frame to the desk top panels to prevent screws penetrating through the panels. Double check the screw sizes."; output={'Aesthetics': 'great looking product', 'Number of Assembly Parts': 'a lot of screws'}
+Now do: comment='''
+        return prompt
+    
+    def preprocess(self, text):
+        text = text.replace('\n', ' ')
+        text = text.replace('"', "")
+        return text
     
     def translate(self, text):
         text = self.preprocess(text)
         task_text =  "下面的文本发生在商品评论的场景:\n" + text + "\n将上面的文本原样翻译为中文："
+        response = self.one_shot(task_text)
+        return response
+    
+    def comment_analyze(self, comment):
+        comment = self.preprocess(comment)
+        task_text = self.comment_analyze_prompt + '"' + comment + '"; output='
+        response = self.one_shot(task_text)
+        return response
+    
+    def points_extract(self, comment):
+        comment = self.preprocess(comment)
+        task_text = self.points_extract_prompt + '"' + comment + '"; output='
         response = self.one_shot(task_text)
         return response
 
